7cb7f9ceffe23cd5943b13fc85f3e135b4552283
unknown <podkanski050@gmail.com>
2023-07-28 16:25:23 +0200

    typed variables in js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const addTask = document.querySelector('#task');
+// -----------------------upper----------------------------
+const typeTask = document.querySelector("#task");
+const addTask = document.querySelector(".btn-add");
+const cardWarning = document.querySelector(".warning");
+// -----------------------bottom---------------------------
+const taskMsg = document.querySelector(".tasks-messange");
+const li = document.querySelectorAll("li");
+const complete = document.querySelectorAll(".complete");
+const edit = document.querySelectorAll(".edit");
+const del = document.querySelectorAll(".delete");
+// ----------------------pop-up----------------------------
+const typeEditTask = document.querySelector("#edit-task");
+const popUpWarning = document.querySelector(".popup__warning");
+const subBtn = document.querySelector(".submit");
+const cancelBtn = document.querySelector(".cancel");
+
